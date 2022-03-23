60f13ecf2b9bd12c000234c7c92ed9dd053f1d64
Root-didi <73555229+Root-didi@users.noreply.github.com>
2022-03-23 01:59:26 +0000

    Create discordv2.sh

---
+++ discordv2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+clear
+
+ 	interface=eth0 ## Change To Your network Interface
+ 	dumpdir=/tmp/
+
+ 		while /bin/true; do
+ 		pkt_old=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ 		sleep 1
+ 		pkt_new=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+
+ 		pkt=$(($pkt_new-$pkt_old))
+ 			echo -ne "\r$pkt packets/s\033[0K"
+      sleep 0.5
+      clear
+ 			if [ $pkt -gt 53240 ]; then  ## If Over 300 Megabits, Alerts Discord Webhook
+ 			echo -e "\n`date` Under attack, dumping packets." 
+ 		
+ 		pktT=$(($pkt/1460))
+ 		
+ 		msg_content=\"Dumping" "$pktT" "Megabytes\" ## Sends The Msg About It Being Hit
+ 		
+ 		url='Discord.com' ## Insert Discord Webhook Here
+ 		
+ 		curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url ## Shows How Much Power Being Put Through
+ 	tcpdump -n -s0 -c 2000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap ## Dumps All Invalid Connected IP's And Forced Connections
+ 
+ echo "`date` Just Got Hit [Sleeping For 1 Min]."
+ 
+ sleep 5
+ 
+ fi
+done
