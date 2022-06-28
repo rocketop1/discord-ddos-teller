99636a1831cf7f0cae42b56fb29e46a4e7781b0d
Root-didi <73555229+Root-didi@users.noreply.github.com>
2022-06-28 17:18:22 +0100

    Update stopddos.sh

--- stopddos.sh
+++ stopddos.sh
@@ -1,33 +1,102 @@
-#!/bin/bash
-clear
+echo -e "RHS"
+echo
+echo "RHS"
+echo
+echo -e "033[97mPackets/s \033[36m{}\n\033[97mBytes/s \033[36m{}\n\033[97mKbp/s \033[36m{}\n\033[97mGbp/s \033[36m{}\n\033[97mMbp/s \033[36m{}"
+interface=eth0
+dumpdir=/root/dumps
+url='https://discord.com/api/webhooks/977633948802883594/iujE9Fsugm3fpet7ucaEgZoAI9NVq0y_sjsXIa_QGdAzmYBEQDHLAkum_JEESkuqFGb7' ## Change this to your Webhook URL
+while /bin/true; do
+  old_b=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $1 }'`
+  
+  old_ps=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+  sleep 1
+  new_b=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $1 }'`
 
- 	interface=eth0 ## Change To Your network Interface
- 	dumpdir=/tmp/
+  new_ps=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+  ##Defining Packets/s
+  pps=$(( $new_ps - $old_ps ))
+  ##Defining Bytes/s
+  byte=$(( $new_b - $old_b ))
 
- 		while /bin/true; do
- 		pkt_old=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
- 		sleep 1
- 		pkt_new=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
 
- 		pkt=$(($pkt_new-$pkt_old))
- 			echo -ne "\r$pkt packets/s\033[0K"
-      sleep 0.5
-      clear
- 			if [ $pkt -gt 53240 ]; then  ## If Over 300 Megabits, Alerts Discord Webhook
- 			echo -e "\n`date` Under attack, dumping packets." 
- 		
- 		pktT=$(($pkt/1460))
- 		
- 		msg_content=\"Dumping" "$pktT" "Megabytes\" ## Sends The Msg About It Being Hit
- 		
- 		url='Discord.com' ## Insert Discord Webhook Here
- 		
- 		curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url ## Shows How Much Power Being Put Through
- 	tcpdump -n -s0 -c 2000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap ## Dumps All Invalid Connected IP's And Forced Connections
- 
- echo "`date` Just Got Hit [Sleeping For 1 Min]."
- 
- sleep 5
- 
- fi
+  echo -ne "\r$pps packets/s\033[0K"
+  tcpdump -n -s0 -c 800 -w $dumpdir/capture.`date +"%Y%m%d-%H%M%S"`.pcap
+  echo "`date` Detecting Attack Packets."
+  sleep 1
+  if [ $pps -gt 2000 ]; then ## Attack alert will display after incoming traffic reach 30000 PPS
+    echo " Attack Detected Monitoring Incoming Traffic"
+    curl -H "Content-Type: application/json" -X POST -d '{
+      "embeds": [{
+      	"inline": false,
+        "title": "Attack Detected On",
+        "username": "Attack Alerts",
+        "color": 15158332,
+         "thumbnail": {
+          "url": "https://media.discordapp.net/attachments/972675304013307905/990835322965352489/unknown.png"
+        },
+         "footer": {
+            "text": "Our system is attempting to mitigate the attack and automatic packet dumping has been activated.",
+            "icon_url": "https://rhscloud.net/templates/RHSCloud/img/RHSSiteLogoWhite.png"
+          },
+    
+        "description": "Detection of an attack ",
+         "fields": [
+      {
+        "name": "**Server Provider**",
+        "value": "RHS",
+        "inline": false
+      },
+      {
+        "name": "**IP Address**",
+        "value": "x.x.x.x",
+        "inline": false
+      },
+      {
+        "name": "**Incoming Packets**",
+        "value": " '$pps' Pps ",
+        "inline": false
+      }
+    ]
+      }]
+    }' $url
+    echo "Paused for."
+    sleep 10  && pkill -HUP -f /usr/sbin/tcpdump  ## The "Attack no longer detected" alert will display in 220 seconds
+    ## echo "Traffic Attack Packets Scrubbed"
+    echo -ne "\r$mbps megabytes/s\033[97"
+    curl -H "Content-Type: application/json" -X POST -d '{
+      "embeds": [{
+      	"inline": false,
+        "title": "Attack Stopped",
+        "username": "  Attack Alerts",
+        "color": 3066993,
+         "thumbnail": {
+          "url": "https://media.discordapp.net/attachments/972675304013307905/990835125749170217/unknown.png"
+        },
+         "footer": {
+            "text": "Our system has mitigated the attack and automatic packet dumping has been deactivated.",
+            "icon_url": "https://rhscloud.net/templates/RHSCloud/img/RHSSiteLogoWhite.png"
+          },    
+          
+        "description": "End of attack",
+         "fields": [
+      {
+        "name": "**Server Provider**",
+        "value": "RHS",
+        "inline": false
+      },
+      {
+        "name": "**IP Address**",
+        "value": "x.x.x.x",
+        "inline": false
+      },
+      {
+        "name": "**Packets**",
+        "value": "'$mbps' Mbps ",
+        "inline": false
+      }
+    ]
+      }]
+    }' $url
+  fi
 done
