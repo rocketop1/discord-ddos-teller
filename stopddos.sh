e4e824023f87a7d27c5d40520e14f2da787540d0
rocketop <149897756+rocketop1@users.noreply.github.com>
2025-01-06 08:38:40 +0530

    Update stopddos.sh

--- stopddos.sh
+++ stopddos.sh
@@ -5,7 +5,7 @@ echo
 echo -e "033[97mPackets/s \033[36m{}\n\033[97mBytes/s \033[36m{}\n\033[97mKbp/s \033[36m{}\n\033[97mGbp/s \033[36m{}\n\033[97mMbp/s \033[36m{}"
 interface=eth0
 dumpdir=/root/dumps
-url='https://discord.com/api/webhooks/' ## Change this to your Webhook URL
+url='https://discord.com/api/webhooks/1325661946908905573/_SEcxvx502hYWAXDGecHuCd0QvnsQeCEH35Pt7wL0Z88wNzx1f5vF6_o_Al0hUxpyec4' ## Change this to your Webhook URL
 while /bin/true; do
   old_b=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $1 }'`
   
